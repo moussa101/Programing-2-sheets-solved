e35254adf1b178064ce86ee109f9b49aafa710e3
moussa101 <abdelrhmanmoussa21@gmail.com>
2024-03-04 21:20:21 +0200

    AS

--- src/DuplicateArrlist.java
+++ src/DuplicateArrlist.java
@@ -12,9 +12,13 @@ public class DuplicateArrlist {
     }
     public static void main(String[] args) {
             Scanner sc= new Scanner(System.in);
-            ArrayList<Integer> nigga = new ArrayList<Integer>();
-            nigga.addAll(Arrays.asList(1,2,3,4,5,5,6,7,7,10));
-        System.out.println(removeDub(nigga));
+            ArrayList<Integer> a = new ArrayList<Integer>();
+         //   a.addAll(Arrays.asList(1,2,3,4,5,5,6,7,7,10));
+            for (int i =0;i<10;i++){
+                int b = sc.nextInt();
+                a.add(b);
+            }
+        System.out.println(removeDub(a));
 
 
     }
